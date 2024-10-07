971ad4ff165a124c43c5418ca8679f133c2d08fd
Sam <samuel.sum@stu.ocsb.ca>
2024-10-07 10:56:59 -0400

    finished task

--- main.js
+++ main.js
@@ -13,3 +13,52 @@
 let it_is_christmas = false
 let teaching_partner = "Mr. Squirrel"
 let number_of_students = 27
+
+
+// READ ME THING
+
+let age = 16;
+
+console.log("I am " + age + " years old");
+
+
+// TASK PART 1
+
+let length = 10;
+
+let width = 5;
+
+let area = length * width;
+
+console.log(area);
+
+console.log("Area: " + area);
+
+
+// TASK PART 2
+
+let first_name = "Santa";
+
+let last_name = "Claws";
+
+last_name = "Claus";
+
+console.log("I still believe in " + first_name + " " + last_name);
+
+
+// TASK PART 3
+
+console.log(typeof area);
+
+console.log(typeof last_name);
+
+
+// TASK PART 4
+
+const π = 3.14;
+
+let radius = 734;
+
+let circle_area = π * radius * radius;
+
+console.log("The area of a circle with a radius of " + radius + " is " + circle_area);
